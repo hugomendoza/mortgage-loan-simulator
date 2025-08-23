d9a77bf21c757ecdb52567c7047272cf1aaee2f5
Hugo <hugo_mendoza_s@outlook.com>
2025-08-23 10:14:39 -0500

    docs: add env.template simulator

--- simulator/src/hooks/useFetch.ts
+++ simulator/src/hooks/useFetch.ts
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
-import { AxiosError } from 'axios';
 import { useMortgageSimulator } from '../store/store';
 import { getMortgages } from '../services';
-import { onCaptureErrorsResponse } from '../utils';
 
 export const useFetch = () => {
   const [loading, setLoading] = useState<boolean>(false);
