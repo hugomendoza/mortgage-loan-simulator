a7cb1a643b6c6381b3ce69b1537d6722bb76c4bc
Hugo <hugo_mendoza_s@outlook.com>
2025-08-23 10:13:38 -0500

    docs: add .env.template examples

---
+++ api/public/assets/Alert-XblQ96_i.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-BGwA03HG.js";import{c as t}from"./createLucideIcon-DBKXZ_PW.js";/**
+ * @license lucide-react v0.540.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],s=t("circle-alert",c);function n({message:r}){return e.jsxs("div",{className:"bg-red-100 text-red-600 rounded-lg p-3 flex justify-center items-center gap-2 text-sm",children:[e.jsx(s,{})," ",r||"No mortgages added yet"]})}export{n as A};
---
+++ api/public/assets/mortgage.service-BLxYFoy6.js
@@ -0,0 +1,1 @@
+import{a}from"./index-BGwA03HG.js";const e=async t=>await a.post("/mortgage",t),g=async()=>await a.get("/mortgage");export{e as c,g};
